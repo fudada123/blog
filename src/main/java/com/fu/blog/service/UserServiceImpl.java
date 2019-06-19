5bb1515f6507368d23ee122a1525f36de72829ab
fudada123 <2210384480@qq.com>
2019-06-20 00:16:01 +0800

    有一点修改

--- src/main/java/com/fu/blog/service/UserServiceImpl.java
+++ src/main/java/com/fu/blog/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.fu.blog.service;
 
 import com.fu.blog.dao.UserRespository;
 import com.fu.blog.po.User;
+import com.fu.blog.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRespository.findByUsernameAndPassword(username,password);
+        User user = userRespository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 
